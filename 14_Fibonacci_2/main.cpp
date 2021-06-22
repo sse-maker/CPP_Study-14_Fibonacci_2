7ff6e69df3205b282b465b77dd04cb11abb14d63
sse-maker <sekwang@postech.ac.kr>
2021-06-22 15:33:49 +0900

    first commit

---
+++ 14_Fibonacci_2/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  14_Fibonacci_2
+//
+//  Created by 세광 on 2021/06/22.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    long long int *arr = new long long int[n + 1];
+    
+    arr[0] = 0;
+    arr[1] = 1;
+    
+    for(int i = 2; i <= n; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    
+    cout << arr[n] << endl;
+    delete [] arr;
+}
